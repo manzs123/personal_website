ae9208be18742e21f4f6ba4aba89cfed53a402fe
Raphael Macayan <111346262+manzs123@users.noreply.github.com>
2024-04-09 20:17:46 +0800

    Commit v2
    Test deploy

--- web-app/src/app/contact/page.js
+++ web-app/src/app/contact/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function About() {
+export default function Contact() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
